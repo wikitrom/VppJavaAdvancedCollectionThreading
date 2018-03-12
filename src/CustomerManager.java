2af5647bf83c95aad2dcc2dfa3edf6d6f4144a7d
Mats Bergstroem <hackingtrom@gmail.com>
2018-03-12 16:52:22 +0100

    MAke CustomerManager thread-safe

--- src/CustomerManager.java
+++ src/CustomerManager.java
@@ -3,37 +3,77 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
-
+import java.util.stream.Collectors;
 
 public class CustomerManager {
 
 	private List<Customer> customers = new ArrayList<Customer>();
 	private int nextId = 0;
 
-	public  void addCustomer(Customer customer) {
+	public void addCustomer(Customer customer) {
+
+		// not thread-safe, make atomic operation
+		// customer.setId(nextId);
+		// nextId++;
+
+		synchronized (this) {
 			customer.setId(nextId);
 			nextId++;
-		customers.add(customer);
+			customers.add(customer);
+		}
 	}
 
 	public void howManyCustomers() {
 		int size = 0;
-		size = customers.size();
+		// might get interference from addCustomer
+		// size = customers.size();
+		synchronized (this) {
+			size = customers.size();
+		}
 		System.out.println("" + new Date() + " : " + size + " customers created");
 	}
 
 	public void displayCustomers() {
-			for (Customer c : customers) {
-				System.out.println(c.toString());
-				try {
-					Thread.sleep(500);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+
+		// this customers collection might change while looping
+		// throwing a ConcurrentModificationException
+		// for (Customer c : customers) {
+		// System.out.println(c.toString());
+		// try {
+		// Thread.sleep(500);
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// }
+
+		// possible solution is to synchronize the looping
+		// but his could have big negative impact on performance
+		
+		// synchronized (this) {
+		// for (Customer c : customers) {
+		// System.out.println(c.toString());
+		// try {
+		// Thread.sleep(500);
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// }
+		// }
+
+		// better solution is to loop over a copy of the original collection
+		// using thread-safe CopyOnWriteArrayList
+		List<Customer> tmpCollection = new CopyOnWriteArrayList<>(customers);
+		for (Customer c : tmpCollection) {
+			System.out.println(c.toString());
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
-
-
 
 }
