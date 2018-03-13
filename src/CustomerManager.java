546a4ebe7d154ac6a4c7ce00a47160a4b7ccd312
Mats Bergstroem <hackingtrom@gmail.com>
2018-03-13 09:15:35 +0100

    Make thread-safe using CopyOnWriteArrayList

--- src/CustomerManager.java
+++ src/CustomerManager.java
@@ -7,36 +7,39 @@ import java.util.stream.Collectors;
 
 public class CustomerManager {
 
-	private List<Customer> customers = new ArrayList<Customer>();
+	// ArrayList is not thread-safe, better use CopyOnWriteArrayList which is
+	// thread-safe
+	// private List<Customer> customers = new ArrayList<Customer>();
+	private List<Customer> customers = new CopyOnWriteArrayList<Customer>();
 	private int nextId = 0;
 
 	public void addCustomer(Customer customer) {
 
-		// not thread-safe, make atomic operation
-		// customer.setId(nextId);
-		// nextId++;
+		// not thread-safe, but working since we changed to use CopyOnWriteArrayList in
+		// constructor. Thus only need to use synchronized for nextId
 
-		synchronized (this) {
+		synchronized (customer) {
 			customer.setId(nextId);
 			nextId++;
-			customers.add(customer);
 		}
+		customers.add(customer);
 	}
 
 	public void howManyCustomers() {
 		int size = 0;
-		// might get interference from addCustomer
-		// size = customers.size();
-		synchronized (this) {
-			size = customers.size();
-		}
+
+		// thread-safe, since we changed to use CopyOnWriteArrayList in
+		// constructor. Thus no need to using synchronized here.
+		size = customers.size();
 		System.out.println("" + new Date() + " : " + size + " customers created");
 	}
 
 	public void displayCustomers() {
 
-		// this customers collection might change while looping
+		// This is not thread/safe
+		// The customers collection might change while looping
 		// throwing a ConcurrentModificationException
+		//
 		// for (Customer c : customers) {
 		// System.out.println(c.toString());
 		// try {
@@ -47,9 +50,9 @@ public class CustomerManager {
 		// }
 		// }
 
-		// possible solution is to synchronize the looping
+		// Possible solution is to synchronize the looping
 		// but his could have big negative impact on performance
-		
+		//
 		// synchronized (this) {
 		// for (Customer c : customers) {
 		// System.out.println(c.toString());
@@ -62,18 +65,20 @@ public class CustomerManager {
 		// }
 		// }
 
-		// better solution is to loop over a copy of the original collection
-		// using thread-safe CopyOnWriteArrayList
-		List<Customer> tmpCollection = new CopyOnWriteArrayList<>(customers);
-		for (Customer c : tmpCollection) {
+		// Solution:
+		// A better solution is to loop over a copy of the original collection
+		// using thread-safe CopyOnWriteArrayList in constructor.
+		// Thus we are back to the original implementation
+
+		for (Customer c : customers) {
 			System.out.println(c.toString());
 			try {
-				Thread.sleep(500);
+				Thread.sleep(200);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+
 	}
 
 }
